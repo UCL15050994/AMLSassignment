fd8fa72930329ef13439950c434a4c0f6bc9cae7
Mohamed Warda <45169582+UCL15050994@users.noreply.github.com>
2019-01-09 11:58:16 +0000

    Finalizing comments

--- task_5.py
+++ task_5.py
@@ -1,50 +1,77 @@
-from joblib import load
-import csv
-import pandas as pd
-import os 
-import cv2
+"""
+ELEC0132: Applied Machine Learning Systems
+Student Number: 15050994
+This module was used to in order to create and write a .csv file for task 5 containing
+all the predicitions of the chosen hair color model on a previously unseen dataset. Specifically,
+the model used is an SVM with a polynomial kernel and C = 0.01 previously trained on RGB values
+from cropped and resized images.
+"""
 
+#Importing relevant packages/modules
+import os
+import cv2
+import csv
+
+from joblib import load
+
+#Path to the folder containing saved models
 models_dir = './models/'
 
+#Path to the folder containing new test set
 images_dir = './unseentestset'
 
+#Sorting the filenames alphanumerically
 filenames = sorted(os.listdir(images_dir))
 
-
+#Creating a list for the raw RGB values
 test_data = []
+
+#Looping through the test set and dynamically populating the
+#test_data array with raw RGB values for each image 
 for file in filenames:
     
+    #Using the imread function to read each image file 
+    #The argument 1 means that the image is NOT grayscaled
     img = cv2.imread('{0}/{1}'.format(images_dir, file), 1)
     
+    #Downsampling the image from 256 x 256 to 128 x 128
     resize_img = cv2.resize(img, (128, 128))
     
+    #Cropping the image to only include the top half for hair color model
     resize_img = resize_img[0:63,0:127]
+    
+    #Appending the raw RGB values to the test data_list
     test_data.append(resize_img)
     
-
-#test_data, failed_data = l2.extract_features('human')
-#
+#Converting test_data to a numpy array
 test_data = np.array(test_data)
+
+#Reshaping the test data into a row in order to test the model on it
 test_data = np.reshape(test_data, (test_data.shape[0], -1))
 
+#No failed data in this case unlike landmarks
 failed_data = []
 
-
+#Name of the saved model associated with the task
 model_name = 'hair_color_final.model'
 
+#Loading the model from the directory
 model = load(models_dir + model_name)
 
+#Using the predict method in order to obtain predictions on the new test set
 predictions = model.predict(test_data)
 
-
+#Test/inference score obtained earlier on the old test set
 accuracy = '82.1%'
 
+#No filtering is required in this case unlike landmarks
 filenames_filtered = []
 
 for filename in filenames:
     if filename not in failed_data:
         filenames_filtered.append(filename)
 
+#Opening and writing a .csv file 
 with open('task_5.csv', 'w', newline = '') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow([accuracy])
