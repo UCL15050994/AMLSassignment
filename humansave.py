b1c36b2744d8f153d76c2812a2959848956ade0d
Mohamed Warda <45169582+UCL15050994@users.noreply.github.com>
2019-01-09 06:33:11 +0000

    Finalizing comments

--- humansave.py
+++ humansave.py
@@ -1,3 +1,13 @@
+"""
+ELEC0132: Applied Machine Learning Systems
+Student Number: 15050994
+
+This module was used to in order to train the best model for human detection and
+save it into a separate file. The best model was found to be an SVM with a linear
+kernel and C = 0.01 trained with facial landmarks.
+"""
+
+#Importing relevant packages/modules
 import os
 import cv2
 import numpy as np
@@ -7,19 +17,28 @@ from joblib import dump
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 
-
+#Facial landmarks are used for human detection
 form = 'landmarks'
 label = 'human'
+
+#Ensuring the correct directory
 ds.l2.images_dir = './images1'
 
+#Extracting the facial landmarks and labels for each image
 X, y = ds.get_data(form, label)
 
+#Data splitting into training data and test data 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
+#Reshaping the arrays into rows before feeding them into an SVM
 X_train = np.reshape(X_train, (X_train.shape[0], -1))
 X_test = np.reshape(X_test, (X_test.shape[0], -1))
 
+#Specifying the classifier with the right hyperparameters
 classifier = svm.SVC(gamma = 'auto', kernel = 'linear', C = 0.01)
 
+#Fitting the data
 classifier.fit(X_train, y_train)
+
+#Saving the classifier in the models folder in the directory
 dump(classifier, 'models/human.model')
